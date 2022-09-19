62a3b073be353f3bc343ab762608042de0c0504b
Rubdh <Benrhu@gmail.com>
2022-09-19 19:09:19 +0200

    Forms added

--- src/js/main.js
+++ src/js/main.js
@@ -2,59 +2,6 @@
 
     'use strict';
 
-    var carousels = function() {
-        $(".owl-carousel1").owlCarousel(
-            {
-                loop:true,
-                center: true,
-                margin:0,
-                responsiveClass:true,
-                nav:false,
-                responsive:{
-                    0:{
-                        items:1,
-                        nav:false
-                    },
-                    600:{
-                        items:1,
-                        nav:false
-                    },
-                    1000:{
-                        items:3,
-                        nav:true,
-                        loop:false
-                    }
-                }
-            }
-        );
-
-        $(".owl-carousel2").owlCarousel(
-            {
-                loop:true,
-                center: false,
-                margin:0,
-                responsiveClass:true,
-                nav:true,
-                responsive:{
-                    0:{
-                        items:1,
-                        nav:false
-                    },
-                    600:{
-                        items:2,
-                        nav:false
-                    },
-                    1000:{
-                        items:3,
-                        nav:true,
-                        loop:true
-                    }
-                }
-            }
-        );
-    }
-
-
     // svg responsive in mobile mode
     var checkPosition = function() {
         if ($(window).width() < 767) {
